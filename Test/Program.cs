fff9412784a7c1766194f9fb94f9d16419aaaee3
Patrick <lmpatrick.frank@gmail.com>
2018-05-29 14:31:47 +0200

    Initial

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,17 +1,103 @@
-﻿using System;
+﻿#region Strategy Interface
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Test
+public enum ePassengerTitle
 {
-    class Program
+    Mr,
+    Mrs,
+    Doctor,
+}
+
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-
-
-        }
+        ePassengerTitle title = ePassengerTitle.Doctor;
+        Context.DoAlgorithm(title);
     }
 }
+
+
+
+public interface IPassengerTitleStrategy
+{
+    void DoAlgorithm();
+}
+
+#endregion
+
+#region Concrete Classes
+
+public class MrPassengerTitleStrategy : IPassengerTitleStrategy
+{
+    #region IPassengerTitleStrategy Members
+
+    public void DoAlgorithm()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
+
+public class MrsPassengerTitleStrategy : IPassengerTitleStrategy
+{
+    #region IPassengerTitleStrategy Members
+
+    public void DoAlgorithm()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
+
+public class DoctorPassengerTitleStrategy : IPassengerTitleStrategy
+{
+    #region IPassengerTitleStrategy Members
+
+    public void DoAlgorithm()
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
+
+#endregion
+
+#region Context
+
+public class Context
+{
+    #region Members
+
+    private static Dictionary<ePassengerTitle, IPassengerTitleStrategy> _strategies =
+        new Dictionary<ePassengerTitle, IPassengerTitleStrategy>();
+
+    #endregion
+
+    #region Ctor
+
+    static Context()
+    {
+        _strategies.Add(ePassengerTitle.Mr, new MrPassengerTitleStrategy());
+        _strategies.Add(ePassengerTitle.Mrs, new MrsPassengerTitleStrategy());
+        _strategies.Add(ePassengerTitle.Doctor, new DoctorPassengerTitleStrategy());
+    }
+
+    #endregion
+
+    #region Methods
+
+    public static void DoAlgorithm(ePassengerTitle title)
+    {
+        _strategies[title].DoAlgorithm();
+    }
+
+    #endregion
+}
+
+#endregion
